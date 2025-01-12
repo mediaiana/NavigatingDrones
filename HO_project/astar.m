4600de5d433d578939886b0e7b3a52d5482a341f
samirertt <samirertt@gmail.com>
2025-01-12 21:07:20 +0300

    astar done

--- HO_project/astar.m
+++ HO_project/astar.m
@@ -1,47 +1,53 @@
-% Define the binary grid
+% define the binary grid
 grid = readmatrix("/Users/AkmatovArtur/Documents/GitHub/NavigatingDrones/maze/gridMap.txt");
 disp(grid);
-% Define start points for the drones
-[startPoints, goal] = runMultipleDronesRRT(); % Each row is a start point [row, column]
 
+
+% Define start points for the drones
+[startPoints, goal] = runMultipleDronesRRT(); % each row is a start point [row, column]
 disp('Starting algorithm...');
-startPoints = startPoints(:, [2, 1]); % Reorder columns: column 2 first, column 1 second
-goal = goal([2, 1]); % Swap row and column for the goal
-% Call the A* algorithm using the MEX function
+
+startPoints = startPoints(:, [2, 1]); % reorder columns: column 2 first, column 1 second
+goal = goal([2, 1]); % swap row and column for the goal
+
+
+% call the A* algorithm using the MEX function
 paths = cppastar(startPoints, goal); % `paths` will be a cell array
 disp('All drones paths found!');
 
 % Adjust paths for MATLAB's 1-based indexing
-adjusted_startPoints = startPoints + 1; % Add 1 to start points
-adjusted_goal = goal + 1;               % Add 1 to goal point
 
-% Adjust each path in the cell array
+adjusted_startPoints = startPoints + 1; % add 1 to start points
+adjusted_goal = goal + 1;               % add 1 to goal point
+
+
+% adjust each path in the cell array
 adjusted_paths = cell(size(paths));
 for i = 1:numel(paths)
     if ~isempty(paths{i})
-        adjusted_paths{i} = paths{i}; % Add 1 to each path
+        adjusted_paths{i} = paths{i}; % add 1 to each path
     else
-        adjusted_paths{i} = []; % Keep empty if no path
+        adjusted_paths{i} = []; % keep empty if no path
     end
 end
 
-% Display the grid
+% display the grid
 figure;
-plotObstacles(grid);      % Visualize the grid
-colormap([1 1 1; 0 0 0]); % Custom colormap: 1->white (free space), 0->black (obstacle)
-axis equal;           % Equal aspect ratio
+plotObstacles(grid);      % visualize the grid
+colormap([1 1 1; 0 0 0]); % custom colormap: 1->white (free space), 0->black (obstacle)
+axis equal;           % equal aspect ratio
 hold on;
 
-% Plot the start and goal points
+% plot the start and goal points
 for i = 1:size(adjusted_startPoints, 1)
-    plot(adjusted_startPoints(i, 2), adjusted_startPoints(i, 1), 'go', 'MarkerSize', 10, 'MarkerFaceColor', 'g'); % Start points
+    plot(adjusted_startPoints(i, 2), adjusted_startPoints(i, 1), 'go', 'MarkerSize', 10, 'MarkerFaceColor', 'g'); % start points
 end
-plot(adjusted_goal(2), adjusted_goal(1), 'bo', 'MarkerSize', 10, 'MarkerFaceColor', 'b'); % Goal point
+plot(adjusted_goal(2), adjusted_goal(1), 'bo', 'MarkerSize', 10, 'MarkerFaceColor', 'b'); % goal point
 
-% Simulation: Explore the grid and animate the process for all paths simultaneously
-max_len = max(cellfun(@length, adjusted_paths)); % Maximum length for the animation loop
+% simulation: Explore the grid and animate the process for all paths simultaneously
+max_len = max(cellfun(@length, adjusted_paths)); % maximum length for the animation loop
 
-% Loop for simultaneous path animation
+% loop for simultaneous path animation
 colors = ['r', 'g', 'b']; % Colors for paths
 for i = 1:max_len
     for j = 1:numel(adjusted_paths)
--- HO_project/cppastar.cpp
+++ HO_project/cppastar.cpp
@@ -8,11 +8,12 @@
 #include <iostream>
 #include <algorithm>
 #include <sstream>
-
-
+#include <vector>
+#include <utility>
+#include <algorithm>
 using namespace std;
 
-
+// a custom compare function for the priority queue (heuristic + cost)
 struct CompareNode_Astar
 {
     bool operator()(const treePath *n1, const treePath *n2) const
@@ -20,13 +21,8 @@ struct CompareNode_Astar
         return (n1->cost + n1->hcost) > (n2->cost + n2->hcost);
     }
 };
-
-#include <vector>
-#include <utility>
-#include <algorithm>
-
-using namespace std;
-
+// returns a list of pairs of x,y axis on the grid which are valid neighbors for a given location in the grid
+// O(1) time complexity
 vector<pair<int, int>> getValidNeighbors(const pair<int, int>& loc, const vector<vector<char>>& grid) {
     if (grid.empty() || grid[0].empty()) {
         return {}; // Return an empty list if the grid is invalid.
@@ -55,13 +51,14 @@ vector<pair<int, int>> getValidNeighbors(const pair<int, int>& loc, const vector
 }
 
 
-
+// calculates the heuristic value between two points
 double heuristic(const pair<int,int>& a, const pair<int,int>& b)
 {
     //eucilean distance
     return sqrt(pow((b.second - a.second),2) + pow((b.first - a.first),2));
 }
 
+// a hash function because pair<int,int> is not hashable by default so i added a custom hash function
 struct hashFunction 
 { 
   size_t operator()(const pair<int,int> &x) const
@@ -70,10 +67,16 @@ struct hashFunction
   } 
 }; 
 
+// A* algorithm implementation
+
+// the time complexity of the whole algorithm is O(alog(n)). a = rows * cols of the grid. 
+// and the total time complexity is O(v * alog(n)) where v is the number of agents.
+// O(N) space complexity 
 vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const pair<int, int> goal)
 {
 
-        // Attempt to open the grid file
+    // reading the grid from the file
+    //holding the grid as a 2D vector of characters
     vector<vector<char>> grid;
     ifstream file("gridMap.txt");
 
@@ -92,14 +95,19 @@ vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const p
     } else {
         mexErrMsgIdAndTxt("AStar:FileError", "Failed to open the file 'gridMap.txt'. Ensure it's in the correct location.");
     }
-    
+
+    // locationCheck is a vector of vectors of pairs of locations at grid all agents. it helps checking if at a specific time (index) an agent is at a specific location.
     vector<vector<pair<int,int>>> locationCheck;
+    
+    // paths is a vector of vectors of pairs of locations at grid all agents. it holds the path of each agent.
     vector<vector<pair<int,int>>> paths;
     paths.resize(startPoint.size());
     locationCheck.resize(startPoint.size());
     
+    //loops until the size is equal to the number of agents
     for(int i = 0; i < startPoint.size(); i++)
     {
+        //data structure to hold the information of the path
         treePath* ans = new treePath;
         ans->path = {};
         ans->location = {0, 0};
@@ -108,7 +116,8 @@ vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const p
 
         unsigned int expandedNodes = 0;
 
-   
+        //a set to hold the visited nodes. so each agent doesn't visit the same node twice
+        // O(1) time complexity
         unordered_set<pair<int, int>, hashFunction> visited;
 
 
@@ -121,14 +130,18 @@ vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const p
             root->location = startPoint[i];
             root->hcost = heuristic(startPoint[i], goal);
 
-    
+            //priority queue to hold the nodes to be expanded
+            // O(log(n)) time complexity n is the number of nodes in the queue.
+            // so the time complexity of the whole algorithm is O(alog(n)). a = rows * cols of the grid. 
             priority_queue<treePath*, vector<treePath*>, CompareNode_Astar> fringe;
             fringe.push(root);
 
             while (!fringe.empty())
             {
                 treePath* stateToExpand = fringe.top();
-
+                
+                //checks if at the specific time (index) an agent is planning to go to a location that is already occupied by another agent at the same time
+                // if so, the agent will wait at the location until one more loop.
                 if(!locationCheck[i].empty()){    
                     for(int j = 0; j < locationCheck[i].size(); j++)
                     {
@@ -141,17 +154,20 @@ vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const p
                 }
                 fringe.pop();
 
+                //adds the location to the locationCheck vector
                 locationCheck[i].push_back(stateToExpand->location);
                 if (expandedNodes > 1e6) {
                     cerr << "Too many nodes expanded. Terminating to avoid excessive memory usage.\n";
                     break;
                 }
+                //checks if the location is already visited
                 if (visited.count(stateToExpand->location) == 0)
                 {
+                    //adds the location to the visited set
                     visited.insert(stateToExpand->location);
                     expandedNodes++;
                     
-
+                    //checks if the location is the goal
                     if (stateToExpand->location == goal)
                     {
                         ans->path = stateToExpand->path;
@@ -160,7 +176,7 @@ vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const p
                         cout << "Agent " << i+1 << " result path size: " << ans->path.size() << endl;
                         delete stateToExpand;
 
-                        
+                        //clears the fringe
                         while (!fringe.empty())
                         {
                             treePath* toDelete = fringe.top();
@@ -169,6 +185,7 @@ vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const p
                         }
                         printf("Expanded nodes: %d\n", expandedNodes);
 
+                        //adds the path to the paths vector
                         paths[i].resize(ans->path.size());
                         for (int k = 0; k < ans->path.size(); k++)
                         {
@@ -177,9 +194,10 @@ vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const p
                     }
                     else
                     {
-
+                        //gets the valid neighbors of the location
                         vector<pair<int, int>> neighbors = getValidNeighbors(stateToExpand->location, grid);
-
+                        
+                        //loops through the neighbors and adds them to the fringe
                         for (int j = 0; j < neighbors.size(); j++)
                         {
                             if (visited.count(neighbors[j]) == 0)
@@ -204,6 +222,7 @@ vector<vector<pair<int, int>>> astar( vector<pair<int, int>> startPoint, const p
     return paths;
 }
 
+//checks if the start and goal points are valid. 0 is a valid location, 1 is an obstacle so if the start or goal points are 1, it is invalid.
 bool isPointsValid(vector<vector<char>>& grid, vector<pair<int, int>> start, pair<int, int> goal)
 {
     for (int i = 0; i < start.size(); i++)
@@ -280,19 +299,21 @@ int main()
 }
 */
 
+// MATLAB interface
 void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
-    if (nrhs != 2) { // Only start points and goal are required as inputs
+    // Check the number of input arguments
+    if (nrhs != 2) { 
         mexErrMsgIdAndTxt("AStar:InvalidInput", "Two inputs required: start points, goal.");
     }
     
 
-    // Convert start points to a vector of pairs
+    // convert start points to a vector of pairs
     size_t numStartPoints = mxGetM(prhs[0]);
     size_t startPointCols = mxGetN(prhs[0]);
     if (startPointCols != 2) {
         mexErrMsgIdAndTxt("AStar:InvalidInput", "Start points must be an Nx2 array.");
     }
-
+    // Convert start points to a vector of pairs
     double* startData = mxGetPr(prhs[0]);
     vector<pair<int, int>> startPoints;
     for (size_t i = 0; i < numStartPoints; ++i) {
@@ -301,24 +322,24 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
         startPoints.emplace_back(x, y);
     }
 
-    // Convert goal point to a pair
+    // convert goal point to a pair
     double* goalData = mxGetPr(prhs[1]);
     pair<int, int> goalPoint = {static_cast<int>(goalData[0]), static_cast<int>(goalData[1])};
 
-    // Run A* algorithm
+    // run A* algorithm
     vector<vector<pair<int, int>>> paths = astar(startPoints, goalPoint);
 
-    // Convert paths to MATLAB output
+    // convert paths to MATLAB output
     plhs[0] = mxCreateCellMatrix(numStartPoints, 1);
     for (size_t i = 0; i < numStartPoints; ++i) {
         const vector<pair<int, int>>& path = paths[i];
         size_t pathSize = path.size();
 
         if (path.empty()) {
-            // Add an empty array for this drone
+            // add an empty array for this drone
             mxSetCell(plhs[0], i, mxCreateDoubleMatrix(0, 0, mxREAL));
         } else {
-            // Create a MATLAB matrix for the path
+            // create a MATLAB matrix for the path
             mxArray* pathArray = mxCreateDoubleMatrix(pathSize, 2, mxREAL);
             double* pathData = mxGetPr(pathArray);
             for (size_t j = 0; j < pathSize; ++j) {
